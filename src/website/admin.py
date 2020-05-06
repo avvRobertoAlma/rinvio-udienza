5d42c1b548f2d9749b63f19a81b7b815b8ecdd15
forlando952 <pisanina82@gmail.com>
2020-05-06 13:12:14 +0200

    tabella giudici

--- src/website/admin.py
+++ src/website/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 from .models import Giudice, Ufficio, Rinvio
 
 
+class RinvioAdmin(admin.ModelAdmin):
+    list_display = ('ufficio','get_cognome_giudice','data_udienza_rinviata','testo')
+    search_fields = ['data_udienza_rinviata', 'testo', 'giudice__cognome']
 
-# Register your models here.
+    def get_cognome_giudice(self, obj):
+        return obj.giudice.cognome
+    
+    get_cognome_giudice.admin_order_field = 'giudice'  # Allows column order sorting
+    get_cognome_giudice.short_description = 'Cognome Giudice'  # Renames column head
+
+
 admin.site.register(Giudice)
 admin.site.register(Ufficio)
-admin.site.register(Rinvio)
+admin.site.register(Rinvio, RinvioAdmin)
+
+ 
+
