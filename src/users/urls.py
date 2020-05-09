b8246cf1f4600ece0967c85fec51529d05b2b576
avvRobertoAlma <roberto.alma@kbl-law.com>
2020-05-09 18:03:37 +0200

    fix logout

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from .views import logout_view
 
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name="login.html"), name="login",),
-    path('logout', logout_view, name="logout"),
+    path('logout', auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 ]
